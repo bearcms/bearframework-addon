1b0f75452dc4932fa80b56126f064ef2c364b153
Ivo Petkov <ivo@ivopetkov.com>
2016-08-25 23:29:01 +0300

    Added `class` attribute.

--- components/bearcms-image-element.php
+++ components/bearcms-image-element.php
@@ -12,11 +12,15 @@ if ($component->onClick === 'fullscreen') {
 } elseif ($component->onClick === 'openUrl') {
     $onClick = 'url';
 }
+$onClickAttribute = ' onClick="' . $onClick . '"';
+
+$class = (string) $component->class;
+$classAttribute = isset($class{0}) ? ' class="' . htmlentities($class) . '"' : '';
 
 $filename = $app->bearCMS->data->getRealFilename($component->filename);
 $content = '<div class="bearcms-image-element">';
 if (isset($filename{0})) {
-    $content .= '<component src="image-gallery" columnsCount="1" onClick="' . $onClick . '">';
+    $content .= '<component src="image-gallery" columnsCount="1"' . $onClickAttribute . $classAttribute . '>';
     $content .= '<file class="bearcms-image-element-image"' . ($onClick === 'url' ? ' url="' . htmlentities($component->url) . '"' : '') . ' title="' . htmlentities($component->title) . '" filename="' . $filename . '"/>';
     $content .= '</component>';
 }
