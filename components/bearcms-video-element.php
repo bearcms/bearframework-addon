744ce0869590a3c99211c3a0c8f602b98512973b
Ivo Petkov <ivo@ivopetkov.com>
2016-09-14 15:06:22 +0300

    Video element HTML output update.

--- components/bearcms-video-element.php
+++ components/bearcms-video-element.php
@@ -35,12 +35,19 @@ if (strlen($component->url) > 0) {
     }
 } elseif (strlen($component->filename) > 0) {
     $filename = $app->bearCMS->data->getRealFilename($component->filename);
-    $content .= '<div class="bearcms-video-element" style="font-size:0;"><video style="width:100%" controls>';
+    $content .= '<div class="bearcms-video-element"><video style="width:100%" controls>';
     $content .= '<source src="' . $app->assets->getUrl($filename) . '" type="video/mp4">';
     $content .= '</video></div>';
 }
 
 $content = \BearCMS\Internal\ElementsHelper::getElementComponentContent($component, 'video', $content);
 ?><html>
+    <head>
+        <style>
+            .bearcms-video-element{
+                font-size:0;
+            }
+        </style>
+    </head>
     <body><?= $content ?></body>
 </html>
