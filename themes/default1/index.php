1a956e41539f38027bcca8eea44dc798ba777aa6
Ivo Petkov <ivo@ivopetkov.com>
2016-10-07 17:04:41 +0300

    Default template hooks update.

--- themes/default1/index.php
+++ themes/default1/index.php
@@ -10,37 +10,27 @@ $context->assets->addDir('themes/default1/assets');
 $app->hooks->add('responseCreated', function($response) use ($app, $context) {
     if (!empty($response->applyBearCMSTemplate)) {
         $templateContent = null;
+        $hookName = null;
         if ($response instanceof App\Response\HTML) {
             $templateContent = '<component src="file:' . $context->dir . '/themes/default1/components/defaultTemplate.php"/>';
+            $hookName = 'bearCMSTheme1DefaultTemplateCreated';
         } elseif ($response instanceof App\Response\NotFound) {
             $templateContent = '<component src="file:' . $context->dir . '/themes/default1/components/defaultTemplate.php" mode="notFound"/>';
+            $hookName = 'bearCMSTheme1NotFoundTemplateCreated';
         } elseif ($response instanceof App\Response\TemporaryUnavailable) {
             $templateContent = '<component src="file:' . $context->dir . '/themes/default1/components/defaultTemplate.php" mode="temporaryUnavailable"/>';
+            $hookName = 'bearCMSTheme1TemporaryUnavailableTemplateCreated';
         }
 
         if ($templateContent !== null) {
             $templateContent = $app->components->process($templateContent, ['recursive' => false]);
             $object = new ArrayObject();
             $object->content = $templateContent;
-            $app->hooks->execute('bearCMSDefaultTheme1Created', $object);
-            $templateContent = $object->content;
-            $templateContent = $app->components->process($templateContent);
-            $object = new ArrayObject();
-            $object->content = $templateContent;
-            $app->hooks->execute('bearCMSDefaultTheme1Ready', $object);
-
-            $content = $response->content;
-            $object = new ArrayObject();
-            $object->content = $content;
-            $app->hooks->execute('bearCMSDefaultTheme1ContentCreated', $object);
-            $content = $object->content;
-            $content = $app->components->process($content);
-            $object = new ArrayObject();
-            $object->content = $content;
-            $app->hooks->execute('bearCMSDefaultTheme1ContentReady', $object);
+            $app->hooks->execute($hookName, $object);
+            $templateContent = $app->components->process($object->content);
 
             $template = new BearFramework\HTMLTemplate($templateContent);
-            $template->insert('body', $app->components->process($response->content));
+            $template->insert('body', $response->content);
             $response->content = $template->getResult();
         }
     }
