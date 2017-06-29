ba5b7b989e50259b006ea94511edb2a8cf77a831
Ivo Petkov <ivo@ivopetkov.com>
2017-06-29 23:25:15 +0300

    Fix in an internal command.

--- classes/BearCMS/Internal/ServerCommands/iconChanged.php
+++ classes/BearCMS/Internal/ServerCommands/iconChanged.php
@@ -7,6 +7,8 @@
  * Free to use under the MIT license.
  */
 
+use BearCMS\Internal\Cookies;
+
 return function() {
     Cookies::setList(Cookies::TYPE_CLIENT, [['name' => 'fc', 'value' => uniqid(), 'expire' => time() + 86400 + 1000]]);
 };
