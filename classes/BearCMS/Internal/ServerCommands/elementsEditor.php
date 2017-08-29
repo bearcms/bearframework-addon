876243ee0578cc3944908633ee6df224bdaa80b1
Ivo Petkov <ivo@ivopetkov.com>
2017-08-29 20:14:49 +0300

    Editor response HTML fix.

--- classes/BearCMS/Internal/ServerCommands/elementsEditor.php
+++ classes/BearCMS/Internal/ServerCommands/elementsEditor.php
@@ -18,6 +18,7 @@ return function($data, $response) {
         $elementsEditorData = Server::call('elementseditor', $requestArguments, true);
         if (is_array($elementsEditorData) && isset($elementsEditorData['result'], $elementsEditorData['result']['content'])) {
             $response['body'] = Server::mergeAjaxResponses($response['body'], json_decode($elementsEditorData['result']['content'], true));
+            $response['body'] = Server::updateAssetsUrls($response['body'], true);
         } else {
             throw new Exception('');
         }
