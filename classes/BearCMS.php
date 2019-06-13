32b0bebf8123b96d76cc2f7fc5915d7c47f89020
Ivo Petkov <ivo@ivopetkov.com>
2019-06-13 09:39:26 +0300

    Updated apple-touch-icon tags.

--- classes/BearCMS.php
+++ classes/BearCMS.php
@@ -1505,7 +1505,9 @@ class BearCMS
         $icon = $settings->icon;
         if (isset($icon{0})) {
             $baseUrl = $this->app->urls->get();
-            $html .= '<link rel="apple-touch-icon-precomposed" href="' . htmlentities($baseUrl . '-link-rel-icon-180') . '">';
+            $html .= '<link rel="apple-touch-icon-precomposed" sizes="180x180" href="' . htmlentities($baseUrl . '-link-rel-icon-180') . '">';
+            $html .= '<link rel="apple-touch-icon-precomposed" sizes="64x64" href="' . htmlentities($baseUrl . '-link-rel-icon-64') . '">';
+            $html .= '<link rel="apple-touch-icon-precomposed" sizes="32x32" href="' . htmlentities($baseUrl . '-link-rel-icon-32') . '">';
             $html .= '<link rel="icon" sizes="192x192" href="' . htmlentities($baseUrl . '-link-rel-icon-192') . '">';
             $html .= '<link rel="icon" sizes="64x64" href="' . htmlentities($baseUrl . '-link-rel-icon-64') . '">';
             $html .= '<link rel="icon" sizes="32x32" href="' . htmlentities($baseUrl . '-link-rel-icon-32') . '">';
