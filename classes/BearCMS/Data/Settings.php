0c5961226dc3a8e89c6a8a58424af9f8df926646
Ivo Petkov <ivo@ivopetkov.com>
2020-02-24 14:56:40 +0200

    Performance optimizations (settings are cached locally)

--- classes/BearCMS/Data/Settings.php
+++ classes/BearCMS/Data/Settings.php
@@ -23,20 +23,24 @@ class Settings
      */
     public function get(): \BearCMS\Data\Settings\Settings
     {
-        $data = Internal\Data::getValue('bearcms/settings.json');
-        if ($data !== null) {
-            $data = json_decode($data, true);
-        } else {
-            $data = [
-                'title' => 'MY COMPANY',
-                'description' => 'The slogan of my company',
-                'languages' => ['en'],
-                'allowSearchEngines' => true,
-                'allowCommentsInBlogPosts' => true,
-                'showRelatedBlogPosts' => true,
-                'externalLinks' => true
-            ];
+        $cacheKey = 'settings';
+        if (!isset(Internal\Data::$cache[$cacheKey])) {
+            $data = Internal\Data::getValue('bearcms/settings.json');
+            if ($data !== null) {
+                $data = json_decode($data, true);
+            } else {
+                $data = [
+                    'title' => 'MY COMPANY',
+                    'description' => 'The slogan of my company',
+                    'languages' => ['en'],
+                    'allowSearchEngines' => true,
+                    'allowCommentsInBlogPosts' => true,
+                    'showRelatedBlogPosts' => true,
+                    'externalLinks' => true
+                ];
+            }
+            Internal\Data::$cache[$cacheKey] = \BearCMS\Data\Settings\Settings::fromArray($data);
         }
-        return \BearCMS\Data\Settings\Settings::fromArray($data);
+        return Internal\Data::$cache[$cacheKey];
     }
 }
