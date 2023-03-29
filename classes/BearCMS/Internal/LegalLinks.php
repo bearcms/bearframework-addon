8eecaaf6cd442e45fe10a7b7e40c6bc660dc85fd
Ivo Petkov <ivo@ivopetkov.com>
2023-03-30 00:15:22 +0300

    Add new internal API for legal links.

---
+++ classes/BearCMS/Internal/LegalLinks.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Bear CMS addon for Bear Framework
+ * https://bearcms.com/
+ * Copyright (c) Amplilabs Ltd.
+ * Free to use under the MIT license.
+ */
+
+namespace BearCMS\Internal;
+
+/**
+ * @internal
+ * @codeCoverageIgnore
+ */
+class LegalLinks
+{
+    static public $links = [];
+}
