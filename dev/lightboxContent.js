e26585567ed3dd896176a8e2c813895152c0a7ac
Ivo Petkov <ivo@ivopetkov.com>
2023-09-22 17:51:23 +0300

    Add 'closeOnEscKey' option to 'bearCMS.lightboxContent.open()'.

--- dev/lightboxContent.js
+++ dev/lightboxContent.js
@@ -16,14 +16,16 @@ bearCMS.lightboxContent = bearCMS.lightboxContent || (function () {
     var open = function (id, options) {
         var options = typeof options !== 'undefined' ? options : {};
         var spacing = typeof options.spacing !== 'undefined' ? options.spacing : '0px';
+        var closeOnEscKey = typeof options.closeOnEscKey !== 'undefined' ? options.closeOnEscKey : true;
         var showCloseButton = typeof options.showCloseButton !== 'undefined' ? options.showCloseButton : true;
         var onOpen = typeof options.onOpen !== 'undefined' ? options.onOpen : null;
         var cache = typeof options.cache !== 'undefined' ? options.cache : false;
         clientPackages.get('lightbox').then(function (lightbox) {
-            var context = lightbox.make({ showCloseButton: showCloseButton });
+            var context = lightbox.make({ closeOnEscKey: closeOnEscKey, showCloseButton: showCloseButton });
             lastLightboxContext = context;
             var open = function (responseText) {
                 context.open(responseText, {
+                    closeOnEscKey: closeOnEscKey,
                     showCloseButton: showCloseButton,
                     spacing: spacing,
                     onOpen: onOpen
